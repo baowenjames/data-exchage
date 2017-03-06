ec5835345a5b4c1cd8f6afd8379546d9c9c5a17b
muyinchen <fei6751803@163.com>
2017-03-06 22:19:47 +0800

    重新整理依赖，并对sys-web消费端进行初步搭建引用并测试成功

--- dc-sys/src/main/java/com/dc/sys/web/SysIndexController.java
+++ dc-sys/src/main/java/com/dc/sys/web/SysIndexController.java
@@ -1,11 +1,15 @@
-package com.dc.web;
+package com.dc.sys.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.dc.sys.facade.entity.SysUser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.dc.sys.facade.facade.SysUserFacade;
 /**
  * <B>系统名称：</B><BR>
  * <B>模块名称：</B><BR>
@@ -16,8 +20,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 @Controller
 public class SysIndexController {
-	
 
+    @Autowired
+    private SysUserFacade sysUserFacade;
 
 	/**
      * <B>方法名称：</B>系统首页<BR>
@@ -31,7 +36,11 @@ public class SysIndexController {
     @RequestMapping("/sysindex.html")
     public ModelAndView index(HttpServletRequest request, HttpServletResponse response) {
         ModelAndView ret = new ModelAndView();
-        
+
+        SysUser user = sysUserFacade.getUser();
+        System.out.println(user.getName());
+
+
         return ret;
     }
     
